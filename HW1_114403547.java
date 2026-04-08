5e145ba9dd38989d71773caa78078c990c903f3a
nkai960806-cloud <nkai960806@gmail.com>
2026-04-08 14:25:29 +0000

    yy

---
+++ HW1_114403547.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class HW1 _114403547
+
+{
+	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+	    
+	    System.out.print("請輸入層數： ");
+	    
+	    int n = scanner.nextInt();
+	    
+	    System.out.print("請選擇類型（1上 2下 3左 4右）： ");
+	    
+	    int type = scanner.nextInt();
+	    
+	    int i,j,k;
+	    
+	    if(type==1){
+	        for(i=1;i<=n;i++){
+	            for(j=1;j<=n-i;j++){
+	                System.out.print(" ");
+	            }
+	            for(k=1;k<=2*i-1;k++){
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	            
+	        }
+	    }
+	    
+	    else if(type==2){
+	        for(i=1;i<=n;i++){
+	            for(j=1;j<=i-1;j++){
+	            System.out.print(" ");
+	        }
+	        for(k=1;k<=2*(n-i)+1;k++){
+	            System.out.print("*");
+	        }
+	        System.out.println();
+	    }
+	    }
+	    else if(type==3){
+	        for(i=1;i<=n;i++){
+	            for(j=1;j<=n-i;j++){
+	                System.out.print(" ");
+	            }
+	            for(k=1;k<=i;k++){
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	            
+	        }
+	        for(i=n-1;i>=1;i--){
+	            for(j=1;j<=n-i;j++){
+	                System.out.print(" ");
+	            }
+	            for(k=1;k<=i;k++){
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+	    }
+	    else if(type==4){
+	        for(i=1;i<=n;i++){
+	            for(k=1;k<=i;k++){
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+	        for(i=n-1;i>=1;i--){
+	            for(k=1;k<=i;k++){
+	                System.out.print("*");
+	            }
+	            System.out.println();
+	        }
+	    
+	    }
+	    
+	    
+	    else{
+	        System.out.print("輸入錯誤！");
+	    }
+	    scanner.close();
+}
+}
